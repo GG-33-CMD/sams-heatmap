0c9bb96526a72399de7d48df9284336ebbc37128
samuel akrasi <sayakrasi2024@outlook.com>
2024-06-09 00:31:00 +0000

    updated the landing page(mobile page is yet to be completed)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var dropdown = document.querySelector('.dropdown');
+    var menuButton = document.getElementById('menu-button');
+    var navMenu = document.getElementById('nav-menu');
+
     dropdown.addEventListener('mouseenter', function() {
         this.querySelector('.dropdown-menu').style.display = 'block';
     });
     dropdown.addEventListener('mouseleave', function() {
         this.querySelector('.dropdown-menu').style.display = 'none';
+    });
+
+    menuButton.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
     });
 });
